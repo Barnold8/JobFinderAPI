cfe51417d711581566ba41ac89b2f42395f36e9a
Brandon Wright <psybw7@nottingham.ac.uk>
2023-08-14 02:20:38 +0100

    Fix: driver title lower, accommodating consistancy

--- classes.py
+++ classes.py
@@ -114,7 +114,7 @@ class JobSite:
         href.URL_encode()
         self.browser.get(href.complete())
         
-        WebDriverWait(self.browser,JobSite.WAIT_TIMER).until(lambda driver: title in driver.title )
+        WebDriverWait(self.browser,JobSite.WAIT_TIMER).until(lambda driver: title in driver.title.lower() )
 
         return self.browser.find_element(By.TAG_NAME,tag)
 
@@ -130,7 +130,7 @@ class Indeed(JobSite):
     def __init__(self,site_params: list[str]) -> None:
         super().__init__()
         self.link = Link("https","indeed","com",site_params)
-        self.website = self.makeRequest(self.link, "Indeed","body")
+        self.website = self.makeRequest(self.link, "indeed","body")
         self.quit()
 
 
