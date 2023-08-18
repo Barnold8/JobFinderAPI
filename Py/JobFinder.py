6e71067433327ec979b44324f378ab6c318f83bb
Brandon Wright <psybw7@nottingham.ac.uk>
2023-08-19 00:58:42 +0100

    Del: remove testing file

--- Py/JobFinder.py
+++ Py/JobFinder.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template
 from flask_restful import reqparse, abort, Api, Resource
-import sys
-sys.path.append("../Py")
+
 import classes
 
 
--- Py/main.py
+++
@@ -1,4 +0,0 @@
-from classes import *
-
-J = TotalJobs(["software","manchester"])
-J.grabPages(2)
